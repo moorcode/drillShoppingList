1a13347ef05825b6ccadfd20058eef8fc667360c
Christina Poole <codenamedchris@gmail.com>
2020-10-29 12:54:06 -0400

    add comments

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const STORE = [
 ];
 
 function toggleDelete(itemId) {
-  const itemIndex = STORE.findIndex(item => item.id === itemId);
-  STORE.splice(itemIndex, 1);
+  const itemIndex = STORE.findIndex(item => item.id === itemId); // find index for item in array where IDs match
+  STORE.splice(itemIndex, 1); // start splice at that index and move 1 to remove item
 }
 
 function toggleCheck(itemId) {
-  const item = STORE.find(item => item.id === itemId);
-  item.checked = !item.checked;
+  const item = STORE.find(item => item.id === itemId); // find item in array where IDs match
+  item.checked = !item.checked; // negate checked value where IDs match
 }
 
 function getItemId(item) {
-  return $(item).closest('li').data('item-id');
+  return $(item).closest('li').data('item-id'); // get 'data-item-id' value from li closest to clicked element
 }
 
 function handleDelete() {
@@ -28,23 +28,23 @@ function handleDelete() {
 }
 
 function handleCheck() {
-  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
-    const id = getItemId(event.currentTarget);
+  $('.js-shopping-list').on('click', `.js-item-toggle`, event => { // listen for check button click on ul
+    const id = getItemId(event.currentTarget); 
     toggleCheck(id);
     renderList();
   });
 }
 
 function addItem(itemName) {
-  STORE.push({id: cuid(), name: itemName, checked: false});
+  STORE.push({id: cuid(), name: itemName, checked: false}); // add new item with user input to array
 }
 
 function handleSubmit() {
-  $('#js-shopping-list-form').submit(function(event) {
-    event.preventDefault();
-    const newItemName = $('.js-shopping-list-entry').val();
-    if (newItemName !== '') {
-      $('.js-shopping-list-entry').val('');
+  $('#js-shopping-list-form').submit(function(event) { // listen for submit event on form
+    event.preventDefault();  // prevent default behavior
+    const newItemName = $('.js-shopping-list-entry').val(); // get value of user input
+    if (newItemName !== '') { // so long as value is not blank (spaces allowed)
+      $('.js-shopping-list-entry').val(''); // clear input field for new entry
       addItem(newItemName);
       renderList();
     }
